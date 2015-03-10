17ef5acde1157e20b5ef2192fc9f4f887b74268e
errormessage <990193251@qq.com>
2015-03-10 16:25:06 +0800

    提交了一个test文件

---
+++ Hibernte_many_2_mang_have/src/testGit/HelloGit.java
@@ -0,0 +1,28 @@
+/*
+ * www.dyr.com
+ * Copyright (c) 2014 All Right Reserved.
+ *    版权					                   保留
+ */
+/**
+ * 
+ */
+package testGit;
+
+/**
+ * Project: Hibernte_many_2_mang_have
+ * Package: testGit
+ * FileName: HelloGit.java 
+ * Comments(意见):
+ * JDK Version:
+ * Author:谢洪章
+ * Create Date: 2015-3-10下午4:24:09
+ * Modified By(修改人):Casper
+ * Modified Time(修改时间):
+ * What is Modified:
+ * Version(版本):
+ */
+public class HelloGit {
+public static void main(String[] args) {
+	System.out.println("Hello Git");
+}
+}
