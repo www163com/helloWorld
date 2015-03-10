558bf7c1bee6cfcf2008334fea377225c8093c59
errormessage <990193251@qq.com>
2015-03-10 16:49:50 +0800

    新建了一个文件

---
+++ TestGit/src/Test1.java
@@ -0,0 +1,37 @@
+/*
+ * www.dyr.com
+ * Copyright (c) 2014 All Right Reserved.
+ *    版权					                   保留
+ */
+/**
+ * 
+ */
+
+/**
+ * Project: TestGit
+ * Package: 
+ * FileName: Test1.java 
+ * Comments(意见):
+ * JDK Version:
+ * Author:谢洪章
+ * Create Date: 2015-3-10下午4:48:52
+ * Modified By(修改人):Casper
+ * Modified Time(修改时间):
+ * What is Modified:
+ * Version(版本):
+ */
+public class Test1 {
+
+	/**
+	 * @author 谢洪章
+	 * Create Time: 2015-3-10下午4:48:52
+	 * Description(描述):
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello Git!!!");
+System.out.println("打印一句话");
+	}
+
+}
